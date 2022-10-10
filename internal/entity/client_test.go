38a343debbe990b6c89a0f97723f557d6cc3893c
joao pedro <joaopedro.lacerda33@gmail.com>
2022-10-10 18:25:13 -0300

    feat: add client create with tests

---
+++ internal/entity/client_test.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := NewClient("john doe", "j@j.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "john doe", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
