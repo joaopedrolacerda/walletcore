c43b8a644bad7028bbc76b9175f9a8ed049cf10c
joao pedro <joaopedro.lacerda33@gmail.com>
2022-10-13 20:12:50 -0300

    feat: update client

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -20,3 +20,15 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
 }
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("john doe", "j@j.com")
+	err := client.Update("John Doe Update", "j@j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "John Doe Update", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("john doe", "j@j.com")
+	err := client.Update("", "j@j.com")
+	assert.Error(t, err, "name is required")
+}
