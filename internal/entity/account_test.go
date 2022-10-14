08de6c6dd90e218f601f098a343604e1511ef073
joao pedro <joaopedro.lacerda33@gmail.com>
2022-10-14 12:42:46 -0300

    feat: add account

---
+++ internal/entity/account_test.go
@@ -0,0 +1,35 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAccount(t *testing.T) {
+	client, _ := NewClient("john Doe", "j@j")
+	account := newAccount(client)
+
+	assert.NotNil(t, account)
+
+	assert.Equal(t, client.ID, account.Client.ID)
+}
+
+func TestCreateAccountWithNilClient(t *testing.T) {
+	account := newAccount(nil)
+	assert.Nil(t, account)
+}
+func TestCreaditAcco(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j")
+	account := newAccount(client)
+	account.Credit(100)
+	assert.Equal(t, float64(100), account.Balance)
+}
+
+func TestDebitAccount(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j")
+	account := newAccount(client)
+	account.Credit(100)
+	account.Debit(50)
+	assert.Equal(t, float64(50), account.Balance)
+}
