e6d5e706dc0a430a2d1236a0c95064f759a5ab12
joao pedro <joaopedro.lacerda33@gmail.com>
2022-10-14 13:02:25 -0300

    feat: add transactions

---
+++ internal/entity/transaction_test.go
@@ -0,0 +1,42 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTransaction(t *testing.T) {
+	client1, _ := NewClient("john Doe", "j@j")
+	Account1 := newAccount(client1)
+
+	client2, _ := NewClient("john doe 2", "j@j2")
+	Account2 := newAccount(client2)
+
+	Account1.Credit(1000)
+	Account2.Credit(1000)
+
+	Transaction, err := newTransaction(Account1, Account2, 100)
+	assert.Nil(t, err)
+	assert.NotNil(t, Transaction)
+	assert.Equal(t, 1100.0, Account2.Balance)
+	assert.Equal(t, 900.0, Account1.Balance)
+}
+
+func TestClientTransactionWithInsuficientBalance(t *testing.T) {
+	client1, _ := NewClient("john Doe", "j@j")
+	Account1 := newAccount(client1)
+
+	client2, _ := NewClient("john doe 2", "j@j2")
+	Account2 := newAccount(client2)
+
+	Account1.Credit(1000)
+	Account2.Credit(1000)
+
+	Transaction, err := newTransaction(Account1, Account2, 2000)
+	assert.NotNil(t, err)
+	assert.Error(t, err, "insuficient funds")
+	assert.Nil(t, Transaction)
+	assert.Equal(t, 1000.0, Account2.Balance)
+	assert.Equal(t, 1000.0, Account1.Balance)
+}
